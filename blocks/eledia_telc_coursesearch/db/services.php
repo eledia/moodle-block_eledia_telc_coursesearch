3d9360dd08110fef46a6094acfbf08690b71cc63
Immanuel Pasanec <ip@eledia.de>
2025-03-11 16:17:30 +0100

    Made core service accessible as service funcion for this plugin. (Hack, because it is only accessible as moodle app service.)

---
+++ blocks/eledia_telc_coursesearch/db/services.php
@@ -0,0 +1,21 @@
+<?php
+$functions = [
+    // The name of your web service function, as discussed above.
+    'block_eledia_telc_coursesearch_get_categories' => [
+        'classname' => 'core_course_external',
+        'methodname' => 'get_categories',
+        'classpath' => 'course/externallib.php',
+        'description' => 'Return category details',
+        'type' => 'read',
+        'capabilities' => 'moodle/category:viewhiddencategories',
+
+        // An optional list of services where the function will be included.
+        /*'services' => [*/
+        /*    // A standard Moodle install includes one default service:*/
+        /*    // - MOODLE_OFFICIAL_MOBILE_SERVICE.*/
+        /*    // Specifying this service means that your function will be available for*/
+        /*    // use in the Moodle Mobile App.*/
+        /*    MOODLE_OFFICIAL_MOBILE_SERVICE,*/
+        /*]*/
+    ],
+];
