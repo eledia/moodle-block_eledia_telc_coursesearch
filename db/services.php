50e073781c1483ae318f202079ebc6aeaa2211da
Immanuel Pasanec <immanuel.pasanec@eledia.de>
2026-01-28 19:16:33 +0100

    Fixes for phpcs errors and new minified JS files.

--- db/services.php
+++ db/services.php
@@ -37,10 +37,8 @@ $functions = [
     'block_eledia_telc_coursesearch_get_available_categories' => [
         'classname' => 'block_eledia_telc_coursesearch\externallib',
         'methodname' => 'get_available_categories',
-        // 'classpath' => 'course/externallib.php',
         'description' => 'Return category details',
         'type' => 'read',
-        // 'capabilities' => 'moodle/category:viewhiddencategories',
         'ajax' => true,
     ],
     'block_eledia_telc_coursesearch_get_available_tags' => [
@@ -53,28 +51,22 @@ $functions = [
     'block_eledia_telc_coursesearch_get_customfield_available_options' => [
         'classname' => 'block_eledia_telc_coursesearch\externallib',
         'methodname' => 'get_customfield_available_options',
-        // 'classpath' => 'course/externallib.php',
         'description' => 'Return category details',
         'type' => 'read',
-        // 'capabilities' => 'moodle/category:viewhiddencategories',
         'ajax' => true,
     ],
     'block_eledia_telc_coursesearch_get_customfields' => [
         'classname' => 'block_eledia_telc_coursesearch\externallib',
         'methodname' => 'get_customfields',
-        // 'classpath' => 'course/externallib.php',
         'description' => 'Return customfields for filter rendering.',
         'type' => 'read',
-        // 'capabilities' => 'moodle/category:viewhiddencategories',
         'ajax' => true,
     ],
     'block_eledia_telc_coursesearch_get_courseview' => [
         'classname' => 'block_eledia_telc_coursesearch\externallib',
         'methodname' => 'get_courseview',
-        // 'classpath' => 'course/externallib.php',
         'description' => 'Return category details',
         'type' => 'read',
-        // 'capabilities' => 'moodle/category:viewhiddencategories',
         'ajax' => true,
         // An optional list of services where the function will be included.
         /*'services' => [*/
